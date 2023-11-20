a81ee794c8af7fdfff18ece671ec48027f338278
Rifas54321 <rifasalappuzhakl04@gmail.com>
2023-11-20 19:53:31 +0530

    update index.js

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ bot.on("message",async(msg)=>{
   }
 })
 //setup
-app.set("view engine","ejs")
 app.use(express.urlencoded({extended:true, limit:"50mb"}));
 app.use(express.json({limit:"50mb"}));
 app.get("/",(req,res)=>{
